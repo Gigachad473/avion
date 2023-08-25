509489b435a77a724204de51f2c74554c57dadfd
Gigachad473 <ilahristoforov88@gmail.com>
2023-08-25 18:46:05 +0200

    New commit

--- public/js/cartConnector.js
+++ public/js/cartConnector.js
@@ -1,24 +1,66 @@
-const cartInner = document.querySelector(".modal-body")
+const cartInner = document.querySelector(".modal-body");
 let cartData = localStorage.getItem("cart");
-const cartArray = JSON.parse(cartData);
+if (cartData) {
+  cartData = JSON.parse(cartData);
+} else {
+  cartData = [];
+}
 
-cartInner.innerHTML = `` 
-// Your cartContent
-const productTitle = cartArray[0].productTitle;
-const productAmount = cartArray[0].productAmount;
+cartInner.innerHTML = `Your cart is empty`;
 
-// Create two <p> elements
-const divParagraph = document.createElement('div')
-const titleParagraph = document.createElement('p');
-const amountParagraph = document.createElement('p');
+function updateCart() {
+  // Clear the cartInner
+  if (cartData !== null && cartData.length > 0) {
+    // Code to execute if cartData is not null and not an empty array
+    cartInner.innerHTML = ``;
+    console.log("some");
+} else {
+  cartInner.innerHTML = `Your cart is empty`;
 
-// Set the text content of the <p> elements
-titleParagraph.textContent = productTitle;
-amountParagraph.textContent = productAmount;
-divParagraph.appendChild(titleParagraph)
-divParagraph.appendChild(amountParagraph)
-divParagraph.classList.add('cart_product')
+}
 
+  // Update the localStorage with the modified cartData
+  localStorage.setItem("cart", JSON.stringify(cartData));
 
-// Append the <p> elements to the cartInner element
-cartInner.appendChild(divParagraph);
+  // Loop through each item in cartData
+  cartData.forEach((cartItem, index) => {
+    const productTitle = cartItem.productTitle;
+    const productAmount = cartItem.productAmount;
+
+    // Create a div to hold the item information
+    const divParagraph = document.createElement('div');
+    divParagraph.classList.add('cart_product');
+
+    // Create <p> elements for the title and amount
+    const titleParagraph = document.createElement('p');
+    const amountParagraph = document.createElement('p');
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add("delete");
+    deleteButton.textContent = `x`;
+
+    // Set the text content of the <p> elements
+    titleParagraph.textContent = productTitle;
+    amountParagraph.textContent = productAmount;
+
+    // Add a click event listener to the delete button
+    deleteButton.addEventListener('click', () => {
+      // Remove the item from cartData
+      cartData.splice(index, 1);
+
+      // Update the cart on the webpage and localStorage
+      updateCart();
+      // location.reload()
+    });
+
+    // Append the <p> elements to the div
+    divParagraph.appendChild(titleParagraph);
+    divParagraph.appendChild(amountParagraph);
+    divParagraph.appendChild(deleteButton);
+
+    // Append the div to the cartInner element
+    cartInner.appendChild(divParagraph);
+  });
+}
+
+// Initial rendering of the cart
+updateCart();
--- public/js/lamp.js
+++ public/js/lamp.js
@@ -4,9 +4,9 @@ const productAmount = document.getElementById("productAmount");
 const productTitle = document.querySelector(".product_title").innerHTML;
 const addToCart = document.querySelector(".product_add");
 const cartInner = document.querySelector(".modal-body");
+const productPrice = document.querySelector(".product_price").innerHTML
 window.onload = productAmount.innerHTML = `1`
 let cartData = localStorage.getItem("cart");
-
 if (cartData) {
   cartData = JSON.parse(cartData);
 } else {
@@ -30,6 +30,7 @@ function updateCartData() {
   cartData.push({
     productTitle: productTitle,
     productAmount: parseInt(productAmount.innerHTML),
+    productPrice: productPrice
   });
 addToCart.innerHTML = `Added`
 addToCart.disabled = true
@@ -64,20 +65,55 @@ addToCart.addEventListener("click", function () {
 });
 
 
-// Display cart contents
-if (cartData.length > 0) {
-  cartData.forEach(item => {
+
+cartInner.innerHTML = ``;
+
+function updateCart() {
+  // Clear the cartInner
+  cartInner.innerHTML = ``;
+
+  // Update the localStorage with the modified cartData
+  localStorage.setItem("cart", JSON.stringify(cartData));
+
+  // Loop through each item in cartData
+  cartData.forEach((cartItem, index) => {
+    const productTitle = cartItem.productTitle;
+    const productAmount = cartItem.productAmount;
+
+    // Create a div to hold the item information
     const divParagraph = document.createElement('div');
-    const titleParagraph = document.createElement('p');
-    const amountParagraph = document.createElement('p');
-
-    titleParagraph.textContent = item.productTitle;
-    amountParagraph.textContent = item.productAmount;
-
-    divParagraph.appendChild(titleParagraph);
-    divParagraph.appendChild(amountParagraph);
     divParagraph.classList.add('cart_product');
 
+    // Create <p> elements for the title and amount
+    const titleParagraph = document.createElement('p');
+    const amountParagraph = document.createElement('p');
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add("delete");
+    deleteButton.textContent = `x`;
+
+    // Set the text content of the <p> elements
+    titleParagraph.textContent = productTitle;
+    amountParagraph.textContent = productAmount;
+
+    // Add a click event listener to the delete button
+    deleteButton.addEventListener('click', () => {
+      // Remove the item from cartData
+      cartData.splice(index, 1);
+
+      // Update the cart on the webpage and localStorage
+      updateCart();
+      location.reload()
+    });
+
+    // Append the <p> elements to the div
+    divParagraph.appendChild(titleParagraph);
+    divParagraph.appendChild(amountParagraph);
+    divParagraph.appendChild(deleteButton);
+
+    // Append the div to the cartInner element
     cartInner.appendChild(divParagraph);
   });
 }
+
+// Initial rendering of the cart
+updateCart();
