dd3a84941f13ddc194fade5c9176a327ba928816
Gigachad473 <ilahristoforov88@gmail.com>
2023-09-21 18:36:32 +0200

    New commit

--- server.js
+++ server.js
@@ -1,256 +1,361 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv')
-require('dotenv').config()
-const mysql = require('mysql2');
-const schedule = require('node-schedule');
-const fs = require('fs');
-const nodemailer = require('nodemailer');
-const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+require("dotenv").config();
+const mysql = require("mysql2");
+const schedule = require("node-schedule");
+const fs = require("fs");
+const nodemailer = require("nodemailer");
+const path = require("path");
 const app = express();
 const port = 3000;
-const stripe = require('stripe')(`${process.env.STRIPE_SECRET_TEST}`);
-
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_TEST}`);
+const bcrypt = require("bcrypt");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  service: "Gmail",
 
-    auth: {
-      user: process.env.EMAIL_ACCOUNT,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  auth: {
+    user: process.env.EMAIL_ACCOUNT,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 //Routing
 
-
 app.use(bodyParser.json());
-app.use(express.static('public'));
-let staticPath = path.join(__dirname, "public")
-app.get('/', (req, res) => {
-    res.sendFile(path.join(staticPath, "index.html"))
-})
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+let staticPath = path.join(__dirname, "public");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(staticPath, "index.html"));
+});
 app.get("/the-lucy-lamp", (req, res) => {
-    res.sendFile(path.join(`${staticPath}/products`, "lamp.html"))
-
-})
+  res.sendFile(path.join(`${staticPath}/products`, "lamp.html"));
+});
 app.get("/the-dandy-chair", (req, res) => {
-    res.sendFile(path.join(`${staticPath}/products`, "chair.html"))
-
-})
+  res.sendFile(path.join(`${staticPath}/products`, "chair.html"));
+});
 app.get("/rustic-vase-set", (req, res) => {
-  res.sendFile(path.join(`${staticPath}/products`, "vaseset.html"))
-})
+  res.sendFile(path.join(`${staticPath}/products`, "vaseset.html"));
+});
 app.get("/the-silky-vase", (req, res) => {
-  res.sendFile(path.join(`${staticPath}/products`, "vase.html"))
-})
+  res.sendFile(path.join(`${staticPath}/products`, "vase.html"));
+});
 app.get("/checkout", (req, res) => {
-  res.sendFile(path.join(staticPath, "checkout.html"))
-})
+  res.sendFile(path.join(staticPath, "checkout.html"));
+});
 app.get("/unsubscribe", (req, res) => {
-  res.sendFile(path.join(staticPath, "unsubscribe.html"))
-})
-
+  res.sendFile(path.join(staticPath, "unsubscribe.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(staticPath, "login.html"));
+});
 
 //DB
 
-
 db.connect((err) => {
-    if (err) {
-      console.error('MySQL connection error:', err);
-    } else {
-      console.log('Connected to MySQL database');
-    }
-  });
-  const mailer = async function (title, obj) {
-    try {
-      const email = "Your static email text here"; // Replace with your static email text
-      // const text = replaceHTML(email, obj);
-  
-      db.query('SELECT email FROM subscriptions', (err, results) => {
-        if (err) {
-          console.error('MySQL query error:', err);
-        } else {
-          results.forEach((row) => {
-            const recipientEmail = row.email;
-  
-            transporter.sendMail(
-              {
-                from: `${process.env.contactEmail} <${process.env.contactEmail}>`,
-                to: recipientEmail,
-                subject: title,
-                replyTo: process.env.contactEmail,
-                headers: { 'Mime-Version': '1.0', 'X-Priority': '3', 'Content-type': 'text/html; charset=iso-8859-1' },
-                html: email, // Use the static email text here
-              },
-              (err) => {
-                if (err) {
-                  console.error('Email sending error:', err);
-                } else {
-                  console.log(`Email sent to ${recipientEmail} at ${new Date().toISOString()}`);
-                }
-              }
-            );
-          });
-        }
-      });
-    } catch (e) {
-      console.error('Error reading email template:', e);
-    }
-  };
-  // Subscription route
-app.post('/subscribe/email', async (req, res) => {
-    const email = req.body.email;
+  if (err) {
+    console.error("MySQL connection error:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+const mailer = async function (title, obj) {
+  try {
+    const email = "Your static email text here"; // Replace with your static email text
+    // const text = replaceHTML(email, obj);
 
-    // Check if the email exists in the database
-    db.query('SELECT * FROM subscriptions WHERE email = ?', [email], (err, results) => {
+    db.query("SELECT email FROM subscriptions", (err, results) => {
       if (err) {
-        console.error('MySQL query error:', err);
-        res.status(500).json({ message: 'Error saving your email', code: '02' });
+        console.error("MySQL query error:", err);
+      } else {
+        results.forEach((row) => {
+          const recipientEmail = row.email;
+
+          transporter.sendMail(
+            {
+              from: `${process.env.contactEmail} <${process.env.contactEmail}>`,
+              to: recipientEmail,
+              subject: title,
+              replyTo: process.env.contactEmail,
+              headers: {
+                "Mime-Version": "1.0",
+                "X-Priority": "3",
+                "Content-type": "text/html; charset=iso-8859-1",
+              },
+              html: email, // Use the static email text here
+            },
+            (err) => {
+              if (err) {
+                console.error("Email sending error:", err);
+              } else {
+                console.log(
+                  `Email sent to ${recipientEmail} at ${new Date().toISOString()}`
+                );
+              }
+            }
+          );
+        });
+      }
+    });
+  } catch (e) {
+    console.error("Error reading email template:", e);
+  }
+};
+// Subscription route
+app.post("/subscribe/email", async (req, res) => {
+  const email = req.body.email;
+
+  // Check if the email exists in the database
+  db.query(
+    "SELECT * FROM subscriptions WHERE email = ?",
+    [email],
+    (err, results) => {
+      if (err) {
+        console.error("MySQL query error:", err);
+        res
+          .status(500)
+          .json({ message: "Error saving your email", code: "02" });
       } else if (results.length === 0) {
         // Email doesn't exist, validate and add to the database
         if (validateEmail(email)) {
-          db.query('INSERT INTO subscriptions (email) VALUES (?)', [email], (err) => {
-            if (err) {
-              console.error('MySQL query error:', err);
-              res.status(500).json({ message: 'Error saving your email', code: '02' });
-            } else {
-              // Send the "hello" email immediately
-              transporter.sendMail({
-                from: `${process.env.contactEmail} <${process.env.contactEmail}>`,
-                to: email,
-                subject: "Welcome email",
-                replyTo: process.env.contactEmail,
-                headers: { 'Mime-Version': '1.0', 'X-Priority': '3', 'Content-type': 'text/html; charset=iso-8859-1' },
-                html: `Welcome to our newsletter,
-                You can unsubscribe by clicking here: <a href="https://avion-l631.onrender.com/unsubscribe">Unsubscribe</a>`
-              })
-              
-              res.status(200).json({ message: 'User has subscribed', code: '03' });
+          db.query(
+            "INSERT INTO subscriptions (email) VALUES (?)",
+            [email],
+            (err) => {
+              if (err) {
+                console.error("MySQL query error:", err);
+                res
+                  .status(500)
+                  .json({ message: "Error saving your email", code: "02" });
+              } else {
+                // Send the "hello" email immediately
+                transporter.sendMail({
+                  from: `${process.env.contactEmail} <${process.env.contactEmail}>`,
+                  to: email,
+                  subject: "Welcome email",
+                  replyTo: process.env.contactEmail,
+                  headers: {
+                    "Mime-Version": "1.0",
+                    "X-Priority": "3",
+                    "Content-type": "text/html; charset=iso-8859-1",
+                  },
+                  html: `Welcome to our newsletter,
+                You can unsubscribe by clicking here: <a href="https://avion-l631.onrender.com/unsubscribe">Unsubscribe</a>`,
+                });
+
+                res
+                  .status(200)
+                  .json({ message: "User has subscribed", code: "03" });
+              }
             }
-          });
+          );
         } else {
-          res.status(400).json({ message: 'Not a valid email', code: '02' });
+          res.status(400).json({ message: "Not a valid email", code: "02" });
         }
       } else {
-        res.status(201).json({ message: 'User Already Subscribed', code: '02' });
+        res
+          .status(201)
+          .json({ message: "User Already Subscribed", code: "02" });
       }
-    });
-  });
-  // Check if an email exists in the database
-app.get('/subscribe/check/:email', (req, res) => {
-    const email = req.params.email;
-  
-    // Perform a database query to check if the email exists
-    db.query('SELECT * FROM subscriptions WHERE email = ?', [email], (err, results) => {
+    }
+  );
+});
+// Check if an email exists in the database
+app.get("/subscribe/check/:email", (req, res) => {
+  const email = req.params.email;
+
+  // Perform a database query to check if the email exists
+  db.query(
+    "SELECT * FROM subscriptions WHERE email = ?",
+    [email],
+    (err, results) => {
       if (err) {
-        console.error('MySQL query error:', err);
-        res.status(500).json({ message: 'Error checking email', code: '02' });
+        console.error("MySQL query error:", err);
+        res.status(500).json({ message: "Error checking email", code: "02" });
       } else {
         if (results.length === 0) {
           // Email doesn't exist, return a response indicating it's not found
-          res.status(404).json({ message: 'Email not found', code: '01' });
+          res.status(404).json({ message: "Email not found", code: "01" });
         } else {
           // Email exists, return a response indicating it's found
-          res.status(200).json({ message: 'Email found', code: '03' });
+          res.status(200).json({ message: "Email found", code: "03" });
         }
       }
-    });
-  });
-  
-  // Unsubscribe route
-  app.post("/unsubscribe", (req, res) => {
-    const { email } = req.body;
-    console.log(email);
-    const query = 'SELECT * FROM subscriptions WHERE email = ?';
-
-    db.query(query, [email], (error, results) => {
-      if (error) {
-        console.error('Database error:', error);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-  
-      if (results.length === 0) {
-        // User not found in the database
-        return res.status(422).json({ error: 'User not found' });
-      }
-  
-      // User found, handle the unsubscribe process
-      // ...
-      // Send a success response if necessary
-      return res.status(200).json({ message: 'Unsubscribe successful' });
-    });
-
-    // Check if the email exists in the database and delete the row
-    const sql = "DELETE FROM subscriptions WHERE email = ?";
-    db.query(sql, [email], (err, result) => {
-        if (err) {
-            console.error("Error unsubscribing:", err);
-            res.status(500).send("Error unsubscribing.");
-        } else if (result.affectedRows === 0) {
-            // Email not found in the database
-            res.status(404).send("Email not found in the database.");
-        } else {
-            res.send(`Unsubscribed email: ${email}`);
-        }
-    });
+    }
+  );
 });
-  
 
-  
-  
-  // Schedule the email sending job
-  schedule.scheduleJob('00 58 11 * * 3', () => {
-    mailer('This is our Subscription Email', { content: "Hello, welcome to our email 👋" });
-  });
-  
-  
-  // Utility function to validate email
-  const validateEmail = (email) => {
-    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(email);
-  };
-  
-  // Utility function to replace placeholders in HTML
-  const replaceHTML = (html, replacements) => {
-    for (const key in replacements) {
-      const regex = new RegExp(`{{${key}}}`, 'g');
-      html = html.replace(regex, replacements[key]);
+// Unsubscribe route
+app.post("/unsubscribe", (req, res) => {
+  const { email } = req.body;
+  console.log(email);
+  const query = "SELECT * FROM subscriptions WHERE email = ?";
+
+  db.query(query, [email], (error, results) => {
+    if (error) {
+      console.error("Database error:", error);
+      return res.status(500).json({ error: "Internal server error" });
     }
-    return html;
-  };
-  app.post('/charge', async (req, res) => {
-    const token = req.body.token;
-    const amount = req.body.amount; // Add this line to receive the payment amount from the frontend
-    console.log(amount)
-  
+
+    if (results.length === 0) {
+      // User not found in the database
+      return res.status(422).json({ error: "User not found" });
+    }
+
+    // User found, handle the unsubscribe process
+    // ...
+    // Send a success response if necessary
+    return res.status(200).json({ message: "Unsubscribe successful" });
+  });
+
+  // Check if the email exists in the database and delete the row
+  const sql = "DELETE FROM subscriptions WHERE email = ?";
+  db.query(sql, [email], (err, result) => {
+    if (err) {
+      console.error("Error unsubscribing:", err);
+      res.status(500).send("Error unsubscribing.");
+    } else if (result.affectedRows === 0) {
+      // Email not found in the database
+      res.status(404).send("Email not found in the database.");
+    } else {
+      res.send(`Unsubscribed email: ${email}`);
+    }
+  });
+});
+
+// Schedule the email sending job
+schedule.scheduleJob("00 58 11 * * 3", () => {
+  mailer("This is our Subscription Email", {
+    content: "Hello, welcome to our email 👋",
+  });
+});
+
+// Utility function to validate email
+const validateEmail = (email) => {
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+};
+
+// Utility function to replace placeholders in HTML
+const replaceHTML = (html, replacements) => {
+  for (const key in replacements) {
+    const regex = new RegExp(`{{${key}}}`, "g");
+    html = html.replace(regex, replacements[key]);
+  }
+  return html;
+};
+app.post("/createUser", async (req, res) => {
+  const user = req.body.email;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  const sqlSearch = "SELECT * FROM userTable WHERE user = ?";
+  const search_query = mysql.format(sqlSearch, [user]);
+  const sqlInsert = "INSERT INTO userTable VALUES (0,?,?)";
+  const insert_query = mysql.format(sqlInsert, [user, hashedPassword]);
+
+  db.query(search_query, async (err, results) => {
+    if (err) {
+      console.error("Error executing SELECT query:", err);
+      return res.sendStatus(500);
+    }
+
+    console.log("------> Search Results");
+    console.log(results.length);
+
+    if (results.length !== 0) {
+      console.log("------> User already exists");
+      return res.sendStatus(409);
+    }
+
+    // User doesn't exist, insert the new user
+    db.query(insert_query, (err, result) => {
+      if (err) {
+        console.error("Error executing INSERT query:", err);
+        return res.sendStatus(500);
+      }
+
+      console.log("--------> Created new User");
+      console.log(result.insertId);
+      res.sendStatus(201);
+      res.redirect("/");
+    });
+  });
+});
+app.post("/userLogin", (req, res) => {
+  const user = req.body.email;
+  const password = req.body.password;
+  console.log(user)
+  console.log(password)
+
+  const sqlSearch = "SELECT * FROM userTable WHERE user = ?";
+  const search_query = mysql.format(sqlSearch, [user]);
+
+  db.query(search_query, async (err, results) => {
+    if (err) {
+      console.error("Error executing SELECT query:", err);
+      return res.sendStatus(500);
+    }
+
+    if (results.length === 0) {
+      console.log("--------> User does not exist");
+      return res.sendStatus(404);
+    }
+
+    const hashedPassword = results[0].password;
+
     try {
-      // Create a charge using the token and the received amount
-      const charge = await stripe.charges.create({
-        amount: amount * 100, // Convert amount to cents (Stripe requires it in cents)
-        currency: 'usd', // Adjust to your currency
-        description: 'Sample Charge',
-        source: token,
-      });
-  
-      // Payment succeeded
-      res.json({ success: true });
+      const passwordMatch = await bcrypt.compare(password, hashedPassword);
+
+      if (passwordMatch) {
+        console.log("---------> Login Successful");
+        res.send(`${user} is logged in!`);
+      } else {
+        console.log("---------> Password Incorrect");
+        res.send("Password incorrect!");
+      }
     } catch (error) {
-      // Payment failed
-      res.json({ success: false, error: error.message });
+      console.error("Error comparing passwords:", error);
+      res.sendStatus(500);
     }
   });
-  
+});
+
+app.post("/charge", async (req, res) => {
+  const token = req.body.token;
+  const amount = req.body.amount; // Add this line to receive the payment amount from the frontend
+  console.log(amount);
+
+  try {
+    // Create a charge using the token and the received amount
+    const charge = await stripe.charges.create({
+      amount: amount * 100, // Convert amount to cents (Stripe requires it in cents)
+      currency: "usd", // Adjust to your currency
+      description: "Sample Charge",
+      source: token,
+    });
+
+    // Payment succeeded
+    res.json({ success: true });
+  } catch (error) {
+    // Payment failed
+    res.json({ success: false, error: error.message });
+  }
+});
+
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
